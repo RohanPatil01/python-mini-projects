de864390d1f8f95ac7529faee4692b876b88db96
Rohan <rohanjpatil63@gmail.com>
2024-03-26 23:16:23 +0530

    Added BMI_Calculator

---
+++ BMI_Calculator.py
@@ -0,0 +1,15 @@
+#BMI Calculator
+weight = int(input("Enter your weight in kgs: "))
+height = int(input("Enter your height in cms: "))
+bmi = round(weight / (height / 100) ** 2, 2)
+
+if bmi <= 18.4:
+    remark = "Underweight"
+elif bmi <= 24.9:
+    remark = "Normal"
+elif bmi <= 39.9:
+    remark = "Overweight"
+else:
+    remark = "Obese"
+
+print(f"Your BMI is {bmi} and you are {remark}.")
